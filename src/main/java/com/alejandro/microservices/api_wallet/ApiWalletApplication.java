2d1f82b08c6837027e8230379cb713f49721826c
Alejandro Calderon <alejodim27@gmail.com>
2025-08-08 18:33:27 -0500

    Initial commit: Proyecto base Spring Boot Wallet API

---
+++ src/main/java/com/alejandro/microservices/api_wallet/ApiWalletApplication.java
@@ -0,0 +1,13 @@
+package com.alejandro.microservices.api_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alejandro/microservices/api_wallet/ApiWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.microservices.api_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
